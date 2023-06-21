84df32eeaad1ba57bc4731d09d7357f1daa7db59
Yaswanth <byaswanth2003@gmail.com>
2023-06-21 13:45:47 +0530

    Refactor calculator code and add input functionality - Update HTML structure, with modified button IDs and Classes - Add Event Listeners for number buttons - Implement input handling and display updates - Add Event Listeners for all-clear & delete buttons - Remove unused code and console.log statements [Ticket: 03]

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ const operate = (num1,operator,num2) =>{
     else if(operator === '/') div(num1,num2)
     else console.log("Type The Correct Operator")
 }
+
+const numbers = Array.from(document.querySelectorAll(".number"))
+const inputContainer = document.querySelector(".input-container")
+let numberInputs =''
+const allClear = document.querySelector("#ac")
+const del = document.querySelector('#del')
+
+allClear.addEventListener('click',()=>{
+    inputContainer.textContent = ''
+    numberInputs = ''
+})
+del.addEventListener('click',()=>{
+    let inputarr = Array.from(numberInputs)
+    inputarr.pop()
+    console.log(inputarr.toString())
+    numberInputs = inputarr.toString().replace(',','')
+    inputContainer.textContent = numberInputs
+})
+numbers.map(number => number.addEventListener('click',()=>{
+    numberInputs = numberInputs + number.textContent
+    inputContainer.textContent = numberInputs
+}))
